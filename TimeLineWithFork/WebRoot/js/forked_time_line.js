a4d88ca5adc5077281182144f386989488c70749
xiaoke_1256 <xiaoke_1256@sina.com>
2022-01-14 23:13:31 +0800

    开始写分支

---
+++ TimeLineWithFork/WebRoot/js/forked_time_line.js
@@ -0,0 +1,16 @@
+(function($){
+  $.fn.extend({
+    forkedTimeLine:function(opts){
+      var datas = [];
+      if (opts instanceof Array){
+    	datas = opts; 
+      }
+      
+      //先整理出各个分支
+      
+      //再给分支分配列
+      
+	  $(this).addClass('time_line');
+    }
+  });
+})(jQuery);
